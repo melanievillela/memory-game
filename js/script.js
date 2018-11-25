ba9a45fca4b4cf8a84cce9e089b64c90eeae979e
Melanie Villela <melanienvillela@gmail.com>
2018-11-25 10:14:44 -0600

    Update
    Removed image files.
    
    Fixed animations.
    
    Updated responsive view.

--- js/script.js
+++ js/script.js
@@ -1,166 +1,186 @@
-const cards = document.querySelectorAll(".card");
-let count = 0;
-const game = document.querySelector(".game");
-let images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png"];
-const modal = document.querySelector(".modal");
-const p = document.createElement("p");
-const reset = document.querySelector("#reset");
-const stars = document.querySelectorAll(".icon");
-let startTime;
-const start = document.querySelector("#start")
-let total = 0;
-const upCards = document.querySelectorAll(".up-card")
+"use strict";
+ 
+ (function init() {
+ 
+   let icons = [`<i class="fa-angry fa-3x far"></i>`, `<i class="fa-dizzy fa-3x far"></i>`, `<i class="fa-grin-stars fa-3x far"></i>`, `<i class="fa-frown fa-3x far"></i>`, 
+                `<i class="fa-grin-tears fa-3x far"></i>`, `<i class="fa-grin-tongue fa-3x far"></i>`, `<i class="fa-laugh fa-3x far"></i>`, `<i class="fa-kiss fa-3x far"></i>`,
+                `<i class="fa-angry fa-3x far"></i>`, `<i class="fa-dizzy fa-3x far"></i>`, `<i class="fa-grin-stars fa-3x far"></i>`, `<i class="fa-frown fa-3x far"></i>`, 
+                `<i class="fa-grin-tears fa-3x far"></i>`, `<i class="fa-grin-tongue fa-3x far"></i>`, `<i class="fa-laugh fa-3x far"></i>`, `<i class="fa-kiss fa-3x far"></i>`]
+    let i, j, tds;
+    let compare = [];
+    let matches = 0;
+    let score = 9;
+    let turns = 0;
 
-//Messages
-const won = document.createTextNode("Congratulations you won! Here are your stats: ");
-const lost = document.createTextNode("Sorry you lost! Click the restart button to try again!");
+    shuffle(icons);
+    createGameboard();
 
-//Shuffle image array
-function shuffle(array) {
-  let currentIndex = images.length;
-  let temp;
-  let randomIndex;
-
-  while (currentIndex != 0) {
-   // Create random index
-   randomIndex = Math.floor(Math.random() * currentIndex);
-   currentIndex -= 1;
-
-   // Swap current index/random index
-   temp = images[currentIndex];
-   images[currentIndex] = images[randomIndex];
-   images[randomIndex] = temp;
- }
- return images;
-}
-//Insert shuffled array into card divs
-function insert(array) {
-  let i = 0;
-  let j = 0;
-  while (i < upCards.length) {
-    upCards[i].innerHTML = "<img src='images/" + images[j] + "'/>";
-    i++;
-    j++;
-  }
-}
-
-//Flip cards over and compare
-function flipCompare() {
-  let compareCardsInner = [];
-  let compareCards = [];
-  for (let k=0; k<cards.length; k++) {
-    let innerHTML;
-    //Listen for click
-    cards[k].addEventListener("click", function() {
-      total++;
-      //Get image source
-      outerElement = this.firstElementChild;
-      innerHTML = this.firstElementChild.innerHTML;
-      //Add to array
-      compareCardsInner.push(innerHTML);
-      compareCards.push(outerElement);
-      //Flip cards over
-      this.firstElementChild.classList.toggle("hide");
-      //Add two flipped cards to array and compare
-      if (compareCardsInner.length === 2) {
-        //Variables to compare cards (innerHTML)
-        let cardOne = compareCardsInner[0];
-        let cardTwo = compareCardsInner[1];
-        //Convert innerHTML to a string to compare
-        let stringOne = cardOne.toString();
-        let stringTwo = cardTwo.toString();
-        if (stringOne === stringTwo) {
-          let  match = 0;
-          compareCards[0].classList.add("match");
-          compareCards[1].classList.add("match");
-          compareCards = [];
-          compareCardsInner = [];
-          //Check to see if all cards on board matched
-          for (let i = 0; i <upCards.length; i++) {
-            if (upCards[i].classList.contains("match")) {
-              match++;
-            }
-            if (match === 16) {
-              const endTime = new Date();
-              const finalTime = endTime.getMinutes() - startTime.getMinutes();
-              modal.classList.remove("hide");
-              modal.appendChild(p);
-              p.appendChild(won);
-              let p2 = p.cloneNode();
-              modal.appendChild(p2);
-              p2.innerHTML = (stars.length) + (" stars with ") + (total) + (" moves in ") + (finalTime) + (" minutes!");
-              }
-            }
-            console.log(match);
-
-          } else {
-            setTimeout(function(){
-              //Add animated shake if no match
-              compareCards[0].classList.add("animated");
-              compareCards[1].classList.add("animated");
-              compareCards[0].classList.add("shake");
-              compareCards[1].classList.add("shake");
-            }, 500);
-            setTimeout(function(){
-              //Remove animated shake and flip the card back over
-              compareCards[0].classList.remove("animated");
-              compareCards[1].classList.remove("animated");
-              compareCards[0].classList.remove("shake");
-              compareCards[1].classList.remove("shake");
-              compareCards[0].classList.add("hide");
-              compareCards[1].classList.add("hide");
-              compareCards = [];
-              compareCardsInner = [];
-            }, 1000);
-            //Check count and remove stars
-            setTimeout(function(){
-            count++;
-            if (count > 3 && count <= 6) {
-              stars[3].classList.add("hide");
-            } else if (count > 6 && count <= 9) {
-              stars[3].classList.add("hide");
-              stars[2].classList.add("hide");
-            } else if (count > 9 && count <= 12) {
-              stars[3].classList.add("hide");
-              stars[2].classList.add("hide");
-              stars[1].classList.add("hide");
-            } else  if (count > 12){
-              stars[3].classList.add("hide");
-              stars[2].classList.add("hide");
-              stars[1].classList.add("hide");
-              stars[0].classList.add("hide");
-              //Show lost modal
-              modal.classList.remove("hide");
-              p.appendChild(lost);
-              modal.appendChild(p);
-            }
-            }, 1800);
-          };
-        };
-      })
+    //Shuffle image array
+    function shuffle(icons) {
+        let currentIndex = icons.length;
+        let temp;
+        let randomIndex;
+    
+        while (currentIndex != 0) {
+        // Create random index
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // Swap current index/random index
+        temp = icons[currentIndex];
+        icons[currentIndex] = icons[randomIndex];
+        icons[randomIndex] = temp;
     }
-  }
-
-  //Restart game
-  reset.addEventListener("click", function() {
-    for (let i = 0; i < upCards.length; i++) {
-      upCards[i].classList.add("hide");
+    return icons;
     }
-    stars[3].classList.remove("hide");
-    stars[2].classList.remove("hide");
-    stars[1].classList.remove("hide");
-    stars[0].classList.remove("hide");
-    shuffle(images);
-    insert(images);
-    count = 0;
-    modal.classList.add("hide");
-  });
 
-  //Timer
-  start.addEventListener("click", function() {
-    startTime = new Date();
-  });
+    //Populate gameboard
+    function createGameboard() {
+        //Create table
+        let table = document.querySelector("table");
+        //Check if table is empty if not clear table
+        if (table.hasChildNodes()) {
+            let rows = document.querySelectorAll("tr");
+            let stars = document.querySelectorAll(".fa-star"); 
+            for (let i = 0; i < rows.length; i++) {
+                rows[i].remove();
+            }
+            for (let j = 0; j < stars.length; j++) {
+                stars[j].classList.remove("hide");
+            }
+        }
+        for (i = 0; i < 4; i++) {
+            let tr = document.createElement("tr");
+            table.appendChild(tr); 
+            for (j = 0; j < 4; j++) {
+                let td = document.createElement("td");
+                tr.appendChild(td);
+            }       
+        }
+        //Add emoji array to table
+        for (i = 0; i < icons.length; i++) {
+            tds = document.querySelectorAll("td");
+            tds[i].innerHTML = icons[i];
+        }
 
-  shuffle(images);
-  insert(images);
-  flipCompare();
+        hideCards();
+    }
+
+    //Hide emojis
+    function hideCards() {
+        let emoji = document.querySelectorAll(".far");
+        for (i = 0; i < icons.length; i++) {
+            emoji[i].classList.add("hide"); 
+        }
+        
+        listenForClick();
+    }
+
+    //Add event listener
+    function listenForClick() {
+        tds = document.querySelectorAll("td");
+        for (i = 0; i < icons.length; i++) {
+            tds[i].addEventListener("click", checkMatch);
+        } 
+    }
+
+    //Check for match
+    function checkMatch() {
+        this.firstChild.classList.remove("hide");
+        compare.push(this.firstChild);
+        console.log(this);
+
+        if (compare.length === 2) {  
+            console.log(compare);
+            turns += 1;    
+            //Remove clicks from other cards    
+            for (i = 0; i < icons.length; i++) {
+                tds[i].removeEventListener("click", checkMatch);
+            }
+
+            //Match
+            if (compare[0].className === compare[1].className) { 
+                //Add animation       
+                compare[0].classList.add("animated", "pulse");
+                compare[1].classList.add("animated", "pulse");              
+                     
+                //Reset array to zero add event listeners back to cards
+                setTimeout(function() {
+                    compare = [];
+                    listenForClick()
+                }, 1250);
+                matches += 1;
+                //Check for win
+                if (matches === 8) {
+                    setTimeout(endGame, {
+                    }, 100);
+                }
+            } 
+
+            //No match    
+            else {
+                score -= 1;
+                compare[0].classList.add("animated", "shake");
+                compare[1].classList.add("animated", "shake");
+
+                //Hide cards, remove animation class remove star and reset array to zero
+                setTimeout(function() {
+                    compare[0].classList.add("hide");
+                    compare[1].classList.add("hide");
+                    compare[0].classList.remove("animated", "shake");
+                    compare[1].classList.remove("animated", "shake");
+                    removeStar(); 
+                    compare = [];
+                    listenForClick()
+                }, 1250);
+            }        
+        }
+    }
+
+    //Remove star
+    function removeStar() {
+        let stars = document.querySelectorAll(".fa-star");
+        if (score === 6) {
+            stars[0].classList.add("hide");
+        } else if (score === 3) {
+            stars[1].classList.add("hide");
+        } else if (score === 0) {
+            stars[2].classList.add("hide");
+            setTimeout(endGame, {            
+            }, 100);
+        } 
+    }
+
+    //Win/Lose messages
+    function endGame() {
+        let modal = document.querySelector("#modal-msg");
+        modal.parentElement.classList.remove("hide");
+        let restart = document.querySelector("button");
+        restart.addEventListener("click", hideModal);
+        if (score === 0) {
+            modal.innerText = 
+            `Sorry you are not a winner. 
+            You lost all your stars and took ${turns} turns.
+            Better luck next time!
+            Play again?`
+        } else {
+            modal.innerText = 
+            `Congratulations you are a winner. 
+            You didn't lose all your stars and took ${turns} turns.
+            Play Again?`
+        }
+    }
+
+    //Hide modal
+    function hideModal() {
+        let modal = document.querySelector("#modal-msg");
+        modal.parentElement.classList.add("hide");
+        //Restart game
+        init();
+    }
+
+})();
+
+
+
+
